7f07da700966fb121ba2dedf6a52a793c99cf686
aakashdeveloper <ahanda205@gmail.com>
2022-11-13 19:55:15 +0530

    stringopt

--- Javascript/14stringoperation.js
+++ Javascript/14stringoperation.js
@@ -73,3 +73,65 @@ city.charAt(0).toUpperCase()+city.slice(1).toLowerCase()
 var city = "paRIs";
 city.charAt(0).toUpperCase()+city.slice(1).toLowerCase()
 'Paris'
+
+var uname = "  Nikita  "
+uname.length
+10
+uname.trim()
+'Nikita'
+
+var uname = "  Nikita .  "
+uname.trim()
+'Nikita .'
+
+var a = "i am learning JavaScript"
+a.replace('JavaScript','JS')
+'i am learning JS'
+
+var a = "JavaScript i am learning JavaScript"
+a.replace('JavaScript','JS')
+'JS i am learning JavaScript'
+a.replace(/JavaScript/g,'JS')
+'JS i am learning JS'
+a.replaceAll('JavaScript','JS')
+'JS i am learning JS'
+
+var uname = "  Nikita .  "
+
+uname.replace(/ /g,'')
+'Nikita.'
+uname.replaceAll(' ','')
+'Nikita.'
+
+var a = ['https:', '', 'github.com', 'Aakashdeveloper', 'edu_intern_aug_22']
+undefined
+a.length
+5
+a[a.length-1]
+'edu_intern_aug_22'
+let b = "https://github.com/Aakashdeveloper/Feb_Node_Mrng_20"
+undefined
+b.split('/')
+(5) ['https:', '', 'github.com', 'Aakashdeveloper', 'Feb_Node_Mrng_20']
+b[b.length-1]
+'0'
+let c = b.split('/')
+undefined
+c[caches.length-1]
+undefined
+c[c.length-1]
+'Feb_Node_Mrng_20'
+
+
+var a = "JavaScript i am learning JavaScript"
+undefined
+a.split(' ')
+(5) ['JavaScript', 'i', 'am', 'learning', 'JavaScript']
+let d= a.split(' ')
+undefined
+d.toString()
+'JavaScript,i,am,learning,JavaScript'
+d.toString().replaceAll(',','')
+'JavaScriptiamlearningJavaScript'
+d.toString().replaceAll(',',' ')
+'JavaScript i am learning JavaScript'
---
+++ Javascript/16array.js
@@ -0,0 +1,7 @@
+Array is a collection of homogeneous data as well as hetrogenious type
+
+var a = ["hi","hello","Hey","test"] >> array of String
+var b = [4,8,56,34,12,59] >> Array of number
+var c = [true,false,false,true] >> Array of boolean
+
+var a = ["hi",5,true,"test",11,13,false]
