9bec79ef933e3a37f141f3d032285d38da49a8bd
aakashdeveloper <ahanda205@gmail.com>
2023-02-12 18:26:39 +0530

    hooks

--- react/zomapp/src/component/Home/QuickSearch.js
+++ react/zomapp/src/component/Home/QuickSearch.js
@@ -1,37 +1,33 @@
-import React,{Component} from 'react';
+import React,{useState,useEffect} from 'react';
 import './QuickSearch.css';
 import QuickDisplay from './QuickDisplay';
 
 const qurl = "http://3.17.216.66:4000/quicksearch"
 
-class QuickSearch extends Component {
-    constructor(){
-        super()
+const QuickSearch = () => {
 
-        this.state={
-            mealType:''
-        }
-    }
-    render(){
-        return(
-            <>
-                <div id="quickSearch">
-                    <span id="quickHeading">Quick Search</span>
-                    <span id="quickSubHeading">Discover Restaurants By Meal</span>
-                    <QuickDisplay mealData={this.state.mealType}/>
-                </div>
-            </>
-        )
-    }
+    const [title] = useState('Quick Search');
+    const [subtitle] = useState('Discover Restaurants By Meal');
+    const [mealData,setMeal] = useState('');
 
-    componentDidMount(){
+    useEffect(() => {
         fetch(`${qurl}`,{method:'GET'})
         .then((res) => res.json())
         .then((data) => {
-            this.setState({mealType:data})
+            setMeal(data)
         })
-    }
-    
+    },[])
+
+
+    return(
+        <>
+                <div id="quickSearch">
+                    <span id="quickHeading">{title}</span>
+                    <span id="quickSubHeading">{subtitle}</span>
+                    <QuickDisplay mealData={mealData}/>
+                </div>
+        </>
+    )
 }
 
 export default QuickSearch
